ca2c5bf73cc49d396dc858f324ba80b0088a6ee4
robomatix <robomatix@robomatix.net>
2015-08-15 13:39:55 +0200

    Affiche 'Configuring application...' et 'Application started...' dans la console js du navigateur

--- 3-app-ultra-basic/js/app/app.js
+++ 3-app-ultra-basic/js/app/app.js
@@ -1,1 +1,16 @@
-console.log('TEST');
+(function () {
+    'use strict';
+
+    var app = angular.module('todo.app', []);
+
+    app.config(appConfig);
+    app.run(appRun);
+
+    function appConfig() {
+        console.log('Configuring application...');
+    }
+
+    function appRun() {
+        console.log('Application started...');
+    }
+})();
